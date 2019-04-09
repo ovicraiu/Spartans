bc7220abf3a6d3ccb8f76f3c45aba56729abc603
iamvalentina <32463819+iamvalentina@users.noreply.github.com>
2019-04-09 18:42:31 +0300

    Mostenire

---
+++ Curs27_Mostenirea/include/CDerivIar.h
@@ -0,0 +1,20 @@
+#ifndef CDERIVIAR_H
+#define CDERIVIAR_H
+#include "CDeriv.h"
+
+
+class CDerivIar:public CDeriv
+{
+    public:
+        CDerivIar();
+        CDerivIar(double);
+        virtual ~CDerivIar();
+        void Afisez();
+
+
+    protected:
+
+    private:
+};
+
+#endif // CDERIVIAR_H
---
+++ Curs27_Mostenirea/src/CDeriv.cpp
@@ -0,0 +1,24 @@
+#include "CDeriv.h"
+#include <iostream>
+
+using namespace std;
+
+CDeriv::CDeriv()
+{
+    cout << "Ctor default CDeriv " << endl;
+}
+
+CDeriv::CDeriv(double parDbl):CBaza(parDbl)
+{
+    cout << "Ctor cu parametru CDeriv " << endl;
+}
+
+
+CDeriv::~CDeriv()
+{
+    cout << "Dtor CDeriv " << endl;
+}
+void CDeriv::Afisez()
+{
+    cout << " Ai apelat metoda Afisez din clasa CDeriv " << endl;
+}
