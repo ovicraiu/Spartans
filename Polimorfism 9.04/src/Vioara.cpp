94ef990f446d50171fedfc96257f26ca9b8f8f58
AnaClaudiaM <claudia.baetica89@gmail.com>
2019-04-11 18:42:21 +0300

    Proiect polimorfism

---
+++ Polimorfism 9.04/src/Vioara.cpp
@@ -0,0 +1,18 @@
+#include "Vioara.h"
+#include"Instrument.h"
+#include<iostream>
+using namespace std;
+void Vioara::RedaNota()
+{
+    cout << " Vioara " << endl;
+}
+
+Vioara::Vioara()
+{
+    //ctor
+}
+
+Vioara::~Vioara()
+{
+    //dtor
+}
